a140cd37f2aa34bf56a6d58f0bc6ccc5449af0f2
Waknis <mihirwaknis282@gmail.com>
2023-11-16 20:33:07 -0800

    Converts DNA Sequence to Protein Sequence

--- dna_to_protein.py
+++ dna_to_protein.py
@@ -12,3 +12,9 @@ protein_sequence = seq.translate()
 # Print the protein sequence
 print("DNA Sequence: ", dna_sequence)
 print("Protein Sequence: ", protein_sequence)
+
+# This would be the output:
+# DNA Sequence:  ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG
+# Protein Sequence:  MAIVMGR*KGAR*
+#In the protein sequence, each letter corresponds to an amino acid (e.g., 'M' for Methionine, 'A' for Alanine),
+# and the '*' symbol represents a stop codon, indicating where the protein sequence ends.
